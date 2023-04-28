38f4524ac8e22093d6fb8998a34e67cd536c1ab9
Mateo Periša <mateoprisa@ssklivno.net>
2023-04-28 09:53:08 +0200

    Dodao sam 2 class-e u models.py/aplikacija

--- my_shop/models.py
+++ my_shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactForm(models.Model):
+    ime = models.CharField(max_length=100)
+    prezime = models.CharField(max_length=100)
+    drzava= models.CharField(max_length=100)
+    subjekt = models.CharField(max_length=100)
+
+
+class StartingPage(models.Model):
+    template_name = "my_shop/index.html"
